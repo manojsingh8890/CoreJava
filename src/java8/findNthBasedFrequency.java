f1b3e1a29f16c0c1d357193f434535a73d66f5fd
mprabhakarsingh <manoj.>
2025-02-11 09:55:35 +0530

    j8

--- src/java8/findNthBasedFrequency.java
+++ src/java8/findNthBasedFrequency.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class findNthBasedFrequency {
@@ -12,15 +13,12 @@ public class findNthBasedFrequency {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> arr = Arrays.asList(1,1,2,3,3,3,4,4,4);
-		Map.Entry<Integer,Long> a = arr.stream().collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+		Map.Entry<Integer,Long> a = arr.stream().collect(Collectors.groupingBy(Function.identity() , Collectors.counting()))
 			      .entrySet().stream()
 			      .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
 			     // .map(Map.Entry::getKey)
 			      .skip(2).findFirst().get();
-		System.out.println(a.getKey() +" : "+ a.getValue());
-		 
+		System.out.println(a.getKey() +" : "+ a.getValue()); 
 		 System.out.println(a);
-		 
-		 
 	}
 }
